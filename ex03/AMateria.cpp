20844dc655d5772c365fe2201623b9110590fafc
Weijiang Yang <weiyang@f4r13s16.clusters.42paris.fr>
2026-03-28 15:41:29 +0100

    ex03

---
+++ ex03/AMateria.cpp
@@ -0,0 +1,19 @@
+#include "AMateria.hpp"
+#include <iostream>
+
+AMateria::AMateria(std::string const &type):_type(type){}
+
+AMateria::~AMateria()
+{
+	std::cout << "AMateria destructor called ." << std::endl;
+}
+
+std::string const & AMateria::getType() const
+{
+	return (_type);
+}
+
+void AMateria::use(ICharacter& target)
+{
+	(void)target;
+}
